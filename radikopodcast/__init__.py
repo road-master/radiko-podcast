890c6999f8c493f5c358c28e263b242f813b7cb4
Master <roadmasternavi@gmail.com>
2023-03-13 03:55:31 +0900

    Bump version: 1.0.1 → 1.0.2

--- radikopodcast/__init__.py
+++ radikopodcast/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Master"""
 __email__ = "roadmasternavi@gmail.com"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
