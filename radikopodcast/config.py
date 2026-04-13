1954d6869792e68d711a7fdd6889dd39888e513b
Master <roadmasternavi@gmail.com>
2026-04-13 23:22:26 +0900

    Fix bug that doesn't work with Python 3.9

--- radikopodcast/config.py
+++ radikopodcast/config.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from dataclasses import field
+from typing import Optional
 
 from yamldataclassconfig.config import YamlDataClassConfig
 
@@ -17,4 +18,5 @@ class Config(YamlDataClassConfig):
     number_process: int = 3
     stop_if_file_exists: bool = False
     keywords: list[str] = field(default_factory=list)
-    radiko_session: str | None = None
+    # Reason: To use auto complete by YamlDataClassConfig
+    radiko_session: Optional[str] = None  # noqa: UP045
